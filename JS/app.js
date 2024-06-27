f1c78d921ee16efb0d8ff10ad6ba1b5b66ebfff7
Alessiasansalone <145045809+Alessiasansalone@users.noreply.github.com>
2024-06-28 00:06:00 +0200

    ADD: es 1

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+/* Esercizio 1
+Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console */
+
+// Dichiarazione variabile
+let number
+// Assegnazione variabile
+number = 100 // number 
+// Stampa in console
+console.log(number)
+
+/* Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+*/
+
+//Dichiarazione variabile
