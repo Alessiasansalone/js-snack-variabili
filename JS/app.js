af8660b5c46b845401d822aac1dfb2b0619463c1
Alessiasansalone <145045809+Alessiasansalone@users.noreply.github.com>
2024-06-28 00:10:17 +0200

    ADD: es2

--- JS/app.js
+++ JS/app.js
@@ -8,8 +8,28 @@ number = 100 // number
 // Stampa in console
 console.log(number)
 
+
 /* Esercizio 2
 Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
 */
 
 //Dichiarazione variabile
+let PI
+// Assegnazione variabile
+PI = '3.14' // string
+// Stampa in console
+console.log(PI)
+
+
+/* Esercizio 2/B
+Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+ */
+
+PI = '3.1416'
+console.log(PI)
+
+/* Esercizio 2/C
+- Dichiara una variabile chiamata radius e assegnale il valore 8.
+- Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+- Stampa il valore della variabile circle in console.
+ */
